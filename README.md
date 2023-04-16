eb8543eed5ea7a151cb93e711067fcd2c4715480
Yanis EL BEILK <yanis@pop-os.localdomain>
2023-04-17 01:12:21 +0200

    restructuring of the project and addition of voting types

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Pour compiler le code source du projet, il faut vous placer à la racine du projet et compiler le projet comme suit :
 ```
-solc -o outputDirectory --bin --ast-compact-json --asm Election.sol    
+solc -o outputDirectory --bin --ast-compact-json --asm src/Election.sol    
 ```
--- src/Election.sol
+++ src/Election.sol
@@ -5,16 +5,22 @@ import "./Ownable.sol";
 import "./SafeMath.sol";
 
 contract Election is Ownable {
+    
+    using SafeMath for uint256;
+    // Store Candidates Count
+    uint public candidatesCount;
+    uint public voteNeutral;
 
+    
     constructor() Ownable(name) {}
-
-using SafeMath for uint256;
-
+    
     // Model a Candidate
     struct Candidate {
         uint256 id;
         string name;
         uint voteCount;
+        uint voteFor;
+        uint voteAgainst;
     }
 
     // Store accounts that have voted
@@ -22,23 +28,26 @@ using SafeMath for uint256;
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
-    // Store Candidates Count
-    uint public candidatesCount;
 
     // voted event
     event votedEvent ( uint indexed _candidateId);
 
     function addCandidate (string memory _name) public onlyOwner {
         candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0, 0, 0);
     }
 
-    function vote (uint _candidateId) public {
+    function vote (uint _candidateId, string memory v) public {
         // require that they haven't voted before
         require(!voters[msg.sender]);
 
         // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
+        
+        // add type of vote
+        if(keccak256(abi.encodePacked(v)) == keccak256(abi.encodePacked("F"))) candidates[_candidateId].voteFor ++;
+        else if (keccak256(abi.encodePacked(v)) == keccak256(abi.encodePacked("A"))) candidates[_candidateId].voteAgainst ++;
+        else voteNeutral++;
 
         // record that voter has voted
         voters[msg.sender] = true;
